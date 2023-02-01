fc8cdfd66200453acdb1e1fc353b4e42ad58669c
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-02-01 22:57:08 +0200

    global style consts

---
+++ client/src/const/styles.js
@@ -0,0 +1,6 @@
+export const latte = "#CBB4A4";
+
+export const border = `2px solid ${latte}`;
+
+export const mainFont = "Space Mono";
+export const mainDisplayFont = "Unbounded";
--- client/src/data/mock.js
+++ client/src/data/mock.js
@@ -3,7 +3,11 @@ export const coffeeshops = [
     id: 1,
     name: "TASTE MAP TECH CAFE",
     address: "Antakalnio g. 17, Vilnius",
-    photoURL: "https://iili.io/H1IuQRa.jpg",
+    photos: [
+      "https://imgur.com/B7nSpbN.jpeg",
+      "https://imgur.com/wjz4fLY.jpeg",
+      "https://imgur.com/SfaQeEV.jpeg",
+    ],
     websites: [
       {
         name: "facebook",
@@ -23,7 +27,11 @@ export const coffeeshops = [
     id: 2,
     name: "HER EXCELLENCY BY VERO CAFE",
     address: "Gedimino pr. 44A, Vilnius",
-    photoURL: "https://iili.io/H1IuvNj.jpg",
+    photos: [
+      "https://imgur.com/Fr31rFO.jpeg",
+      "https://imgur.com/B2SedNf.jpeg",
+      "https://imgur.com/y1MrSlU.jpeg",
+    ],
     websites: [
       {
         name: "facebook",
@@ -43,7 +51,11 @@ export const coffeeshops = [
     id: 3,
     name: "STRANGELOVE",
     address: "Barboros Radvilaites g. 6B, Vilnius",
-    photoURL: "https://iili.io/H1IuriB.jpg",
+    photos: [
+      "https://imgur.com/24y43XO.jpeg",
+      "https://imgur.com/g41iU51.jpeg",
+      "https://imgur.com/mgx9EIN.jpeg",
+    ],
     websites: [
       {
         name: "facebook",
@@ -63,7 +75,10 @@ export const coffeeshops = [
     id: 4,
     name: "STORIES CAFE",
     address: "Nemenčinės pl. 4c, Vilnius",
-    photoURL: "https://iili.io/H1Iu8Dx.jpg",
+    photos: [
+      "https://imgur.com/IBvryG3.jpeg",
+      "https://imgur.com/EPv0LRX.jpeg",
+    ],
     websites: [
       {
         name: "facebook",
@@ -79,7 +94,10 @@ export const coffeeshops = [
     id: 5,
     name: "BACKSTAGE CAFE",
     address: "T.Ševčenkos g. 16h, Vilnius",
-    photoURL: "https://iili.io/H1IukAb.jpg",
+    photos: [
+      "https://imgur.com/i0nz3Dc.jpeg",
+      "https://imgur.com/8qHPD83.jpeg",
+    ],
     websites: [
       {
         name: "facebook",
@@ -92,6 +110,72 @@ export const coffeeshops = [
       {
         name: "homepage",
         link: "https://backstageroasters.com/",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "COFFEE SPELLS",
+    address: "Pylimo g. 38c, Vilnius",
+    photos: [
+      "https://imgur.com/LiQvggk.jpeg",
+      "https://imgur.com/aKG8wXh.jpeg",
+    ],
+    websites: [
+      {
+        name: "facebook",
+        link: "https://www.facebook.com/coffeespells",
+      },
+      {
+        name: "instagram",
+        link: "https://www.instagram.com/coffee.spells/",
+      },
+      {
+        name: "homepage",
+        link: "https://www.coffeespells.lt/",
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "KAVOS REIKALAI",
+    address: "Aukštaičių g. 12, Vilnius",
+    photos: [
+      "https://imgur.com/b7nhZUv.jpeg",
+      "https://imgur.com/yevpRWp.jpeg",
+      "https://imgur.com/zf2XPyx.jpeg",
+    ],
+    websites: [
+      {
+        name: "facebook",
+        link: "https://www.facebook.com/kavosreikalai/?fref=ts",
+      },
+      {
+        name: "instagram",
+        link: "https://www.instagram.com/kavosreikalai/",
+      },
+      {
+        name: "homepage",
+        link: "http://www.kavosreikalai.lt/",
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "ELSKA COFFEE",
+    address: "Pamėnkalnio g. 1",
+    photos: [
+      "https://imgur.com/OfstDiQ.jpeg",
+      "https://imgur.com/liwtA6J.jpeg",
+    ],
+    websites: [
+      {
+        name: "facebook",
+        link: "https://www.facebook.com/elska.coffee",
+      },
+      {
+        name: "instagram",
+        link: "https://www.instagram.com/elskacoffeelt/",
       },
     ],
   },
