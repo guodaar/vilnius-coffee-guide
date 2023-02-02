4f497ff92e3b80b871b72a137e9b53d2afa442d9
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-02-02 22:18:09 +0200

    routes adjustment

--- client/src/routes/const.js
+++ client/src/routes/const.js
@@ -2,33 +2,31 @@ import Home from "../pages/Home/Home";
 import Contact from "../pages/Contact/Contact";
 import Coffeeshops from "../pages/Coffeeshops/Coffeeshops";
 import ShopPage from "../pages/ShopPage/ShopPage";
+import MainLayout from "../layouts/MainLayout";
 
 export const HOME_PATH = "/";
-export const CONTACT_PATH = "/contact";
-export const COFFEESHOPS_PATH = "/coffeeshops";
-export const SHOP_PATH = `${COFFEESHOPS_PATH}:coffeeshop`;
+export const CONTACT_PATH = `${HOME_PATH}contact`;
+export const COFFEESHOPS_PATH = `${HOME_PATH}coffeeshops`;
+export const SHOP_PATH = `${COFFEESHOPS_PATH}/:shopId`;
 
-export const routes = [
-  {
-    path: HOME_PATH,
-    name: "Home",
-    Component: Home,
-  },
-  {
-    path: COFFEESHOPS_PATH,
-    name: "Coffee shops",
-    Component: Coffeeshops,
-  },
-  {
-    path: CONTACT_PATH,
-    name: "Contact",
-    Component: Contact,
-  },
-];
-
-export const secondaryRoutes = [
-  {
-    path: SHOP_PATH,
-    Component: ShopPage,
-  },
-];
+export const mainLayoutRoutes = {
+  Layout: MainLayout,
+  routes: [
+    {
+      path: HOME_PATH,
+      Component: Home,
+    },
+    {
+      path: COFFEESHOPS_PATH,
+      Component: Coffeeshops,
+    },
+    {
+      path: CONTACT_PATH,
+      Component: Contact,
+    },
+    {
+      path: SHOP_PATH,
+      Component: ShopPage,
+    },
+  ],
+};
