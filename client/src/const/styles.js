08baa0d328c25d205f244ac2d90d32cc7e492c54
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-02-05 17:02:47 +0200

    minor style adjustments

--- client/src/const/styles.js
+++ client/src/const/styles.js
@@ -1,5 +1,7 @@
 export const latte = "#CBB4A4";
 export const darkLatte = "#B29E90";
+export const mocha = "#7F6B5D";
+export const milk = "#E5DAD1";
 
 export const border = `2px solid ${latte}`;
 
