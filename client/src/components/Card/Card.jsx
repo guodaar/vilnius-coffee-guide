5ca594a7439819858e45ab7a86e5f060d22c3cf1
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-02-21 22:07:28 +0200

    added stars to shop card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,34 +2,38 @@ import styled from "styled-components";
 import { border, latte, mainFont } from "../../const/styles";
 import { SHOP_PATH } from "../../routes/const";
 import Button from "../Button/Button";
-import {useParams, useNavigate, generatePath} from 'react-router-dom';
+import { useParams, useNavigate, generatePath } from "react-router-dom";
+import Stars from "../ReactStars/ReactStars";
 
-const Card = ({shopName, image, id}) => {
+const Card = ({ shopName, image, id, rating }) => {
   const navigate = useNavigate();
-  const {shop} = useParams();
+  const { shop } = useParams();
 
   const navigateToShop = (shop, shopId) => {
-    const path = generatePath(SHOP_PATH, {shop, shopId});
+    const path = generatePath(SHOP_PATH, { shop, shopId });
     navigate(path);
-  }
+  };
 
-  return <Container >
-    <Title>{shopName}</Title>
-    <CardContent image={image}>
-      <HoverDiv>
-        <Button onClick={() => navigateToShop(shop, id)}>See more</Button>
-      </HoverDiv>
-    </CardContent>
-  </Container>
-}
+  return (
+    <Container>
+      <Title>{shopName}</Title>
+      <CardContent image={image}>
+        <HoverDiv>
+          <Stars rating={rating} />
+          <Button onClick={() => navigateToShop(shop, id)}>See more</Button>
+        </HoverDiv>
+      </CardContent>
+    </Container>
+  );
+};
 
-export default Card
+export default Card;
 
 const Container = styled.div`
   border: ${border};
   display: flex;
   flex-direction: column;
-`
+`;
 
 const Title = styled.h2`
   color: ${latte};
@@ -38,7 +42,7 @@ const Title = styled.h2`
   font-family: ${mainFont};
   padding: 5px;
   flex: 1;
-`
+`;
 
 const CardContent = styled.div`
   min-height: 300px;
@@ -46,16 +50,16 @@ const CardContent = styled.div`
   display: flex;
   justify-content: center;
   position: relative;
-  background: url(${props => props.image}) no-repeat center;
+  background: url(${(props) => props.image}) no-repeat center;
   background-size: cover;
-
-`
+`;
 const HoverDiv = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  padding-top: 70px;
+  gap: 30px;
   width: 100%;
   height: 100%;
   box-sizing: border-box;
@@ -66,4 +70,4 @@ const HoverDiv = styled.div`
   &:hover {
     opacity: 1;
   }
-`
+`;
