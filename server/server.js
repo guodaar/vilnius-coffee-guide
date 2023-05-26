551f674d89b3f4f6d650692c5ff28754b208c02b
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-05-26 15:19:10 +0300

    adjusted path

--- server/server.js
+++ server/server.js
@@ -93,11 +93,11 @@ app.get("/reviews", async (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  //*Set static folder
-  app.use(express.static("client/build"));
+  //* Set static folder
+  app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"))
   );
 }
 
